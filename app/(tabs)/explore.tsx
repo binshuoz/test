add83c9fd7b33a8db752bb90f65b12eda06d8bfd
binshuoz <124227335+binshuoz@users.noreply.github.com>
2024-08-02 13:28:35 +0800

    Update explore.tsx

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -16,6 +16,7 @@ export default function TabTwoScreen() {
         <ThemedText type="title">Explore</ThemedText>
       </ThemedView>
       <ThemedText>This app includes example code to help you get started. This is a test message.</ThemedText>
+      <ThemedText>This is a test message.</ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
           This app has two screens:{' '}
